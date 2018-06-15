83aa4e3b8b8f8192d22f511a249af2d9962e5e37
robpot891 <rpotter@live.ca>
2018-06-15 01:04:03 -0400

    Update lazy_install.sh

--- lazy_install.sh
+++ lazy_install.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install libtiff5-dev libjasper-dev libpng12-dev libjpeg-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libgtk2.0-dev libatlas-base-dev gfortran -y
+sudo apt-get install libtiff5-dev libjasper-dev libpng12-dev libjpeg-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libgtk2.0-dev libatlas-base-dev gfortran git nmap -y
 
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
